19a40964671f618c880a1261d82d587a0c572e73
Fernando Mumbach <fermuch@ayvu.net>
2022-03-12 17:51:12 -0300

    primeira versão do script

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import * as MonoUtils from "@fermuch/monoutils";
 
 // based on settingsSchema @ package.json
-type Config = Record<string, unknown> & {
-  nome: string;
+// type Config = {}
+// const conf = new MonoUtils.config.Config<Config>();
+
+declare class ActivityRecognitionEvent extends MonoUtils.wk.event.BaseEvent {
+  kind: 'activity-recognition';
+  getData(): {kind: string; data: {activityType: string; confidence: number}};
 }
 
-const conf = new MonoUtils.config.Config<Config>();
-
 messages.on('onInit', function() {
-  platform.log('script started! (I am the script)');
-  platform.log('settings:');
-  platform.log(conf.store);
-
-  const name = conf.get('nome', 'default name');
-  platform.log(`Hello, ${name}!`);
+  platform.log('activity recognition script started');
+  
+  MonoUtils.wk.event.subscribe<ActivityRecognitionEvent>('activity-recognition', (ev) => {
+    const data = ev.getData();
+    platform.log('activity recognition event', data);
+    env.project?.saveEvent(ev);
+  });
 });
