2138ffa041cf32487e3adbdd8f3078a69182b4e3
Fernando Mumbach <fermuch@ayvu.net>
2022-03-17 15:22:59 -0300

    adding tests for script

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,4 @@
+import * as MonoUtils from '@fermuch/monoutils';
 const read = require('fs').readFileSync;
 const join = require('path').join;
 
@@ -5,6 +6,24 @@ function loadScript() {
   // import global script
   const script = read(join(__dirname, '..', 'dist', 'bundle.js')).toString('utf-8');
   eval(script);
+}
+
+class MockActivityRecognitionEvent extends MonoUtils.wk.event.BaseEvent {
+  kind = 'activity-recognition' as const;
+
+  constructor(private activityType: string, private confidence: number) {
+    super();
+  }
+  
+  getData() {
+    return {
+      kind: this.kind,
+      data: {
+        activityType: this.activityType,
+        confidence: this.confidence,
+      }
+    };
+  };
 }
 
 describe("onInit", () => {
@@ -18,24 +37,22 @@ describe("onInit", () => {
     messages.emit('onInit');
   });
 
-  it('prints "Hello, default name!"', () => {
-    const log = jest.fn();
-    platform.log = log;
+  it('stores activity events', () => {
+    (env.project as any) = {
+      saveEvent: jest.fn()
+    };
+
+    const ev = new MockActivityRecognitionEvent('STILL', 0.9);
 
     loadScript();
-
     messages.emit('onInit');
-    expect(log).toHaveBeenCalledWith('Hello, default name!');
-  });
+    messages.emit('onEvent', ev);
 
-  it('prints "Hello, custom name!" if given config', () => {
-    const log = jest.fn();
-    platform.log = log;
-    getSettings = () => ({ name: 'custom name' });
-
-    loadScript();
-
-    messages.emit('onInit');
-    expect(log).toHaveBeenCalledWith('Hello, custom name!');
+    expect(env.project.saveEvent).toHaveBeenCalledTimes(1);
+    const call = (env.project.saveEvent as jest.Mock).mock.calls[0];
+    expect(call[0].createdAt).toBeTruthy();
+    expect(call[0].deviceId).toBe("TEST");
+    expect(call[0].kind).toBe("activity-recognition");
+    expect(call[0].loginId).toBe("");
   });
 });
