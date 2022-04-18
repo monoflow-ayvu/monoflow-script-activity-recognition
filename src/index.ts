0252b038995afc4b3e7c6da9a9d14860ef39508e
Fernando Mumbach <fermuch@gmail.com>
2022-04-18 10:48:21 -0300

    bumping time based on activity and only executing for significant changes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as MonoUtils from "@fermuch/monoutils";
+import { currentLogin } from "@fermuch/monoutils";
 
 // based on settingsSchema @ package.json
 // type Config = {}
@@ -9,12 +10,87 @@ declare class ActivityRecognitionEvent extends MonoUtils.wk.event.BaseEvent {
   getData(): {kind: string; data: {activityType: string; confidence: number}};
 }
 
+interface Activity {
+  name: string;
+  confidence: number;
+  since: number;
+}
+
+function setHourmeter(activity: Activity): void {
+  const activitySeconds = (Date.now() - activity.since) / 1000;
+  const hourmeterCol = MonoUtils.collections.getHourmeterDoc();
+  const today = new Date().toISOString().split('T')[0];
+  hourmeterCol.bump(activity.name, activitySeconds);
+  hourmeterCol.bump(`${today}_${activity.name}`, activitySeconds);
+
+  const login = currentLogin();
+  if (login) {
+    const col = env.project?.collectionsManager?.ensureExists?.<MonoUtils.collections.HourmeterCollection>(
+      "hourmeters"
+    );
+    const loginDoc = col?.get(login);
+    if (loginDoc) {
+      loginDoc.bump(activity.name, activitySeconds);
+      loginDoc.bump(`${today}_${activity.name}`, activitySeconds);
+    }
+  }
+}
+
+function currentActivity(): Activity {
+  let lastActivity = {name: 'unknown', confidence: 0} as Activity;
+  try {
+    const lastActivity = data.CURRENT_ACTIVITY as Activity | undefined;
+    if (typeof lastActivity?.name === 'string' && typeof lastActivity?.confidence === 'number') {
+      return lastActivity;
+    }
+  } catch (e) {
+    /* ignore */
+  }
+  
+  return lastActivity;
+}
+
+function setCurrentActivity(name: string, confidence: number): void {
+  const lastActivity = currentActivity();
+  let since = Date.now();
+  if (lastActivity.name === name) {
+    since = lastActivity.since;
+  } else {
+    // save change of activity
+    setHourmeter({name, confidence, since});
+  }
+  const activity = {name, confidence, since} as Activity;
+  env.setData('CURRENT_ACTIVITY', activity);
+}
+
+/**
+ * Check if the activity has a significant change compared to the current activity.
+ * A significant change is defined as a change of at least 30% of the confidence, or
+ * a change of activity name.
+ */
+function isSignificantChange(name: string, confidence: number): boolean {
+  const lastActivity = currentActivity();
+  const lastName = lastActivity.name;
+  const lastConfidence = lastActivity.confidence;
+  const confidenceChange = Math.abs(lastConfidence - confidence);
+  const nameChange = lastName !== name;
+  return nameChange || confidenceChange > 0.3;
+}
+
+function changeHandler(event: ActivityRecognitionEvent): void {
+  const {kind, data} = event.getData();
+
+  if (!isSignificantChange(data.activityType, data.confidence)) {
+    return;
+  }
+
+  platform.log('activity recognition event', data);
+  env.project?.saveEvent(event);
+  setCurrentActivity(data.activityType, data.confidence);
+}
+
 messages.on('onInit', function() {
   platform.log('activity recognition script started');
   
-  MonoUtils.wk.event.subscribe<ActivityRecognitionEvent>('activity-recognition', (ev) => {
-    const data = ev.getData();
-    platform.log('activity recognition event', data);
-    env.project?.saveEvent(ev);
-  });
+  MonoUtils.wk.event.subscribe<ActivityRecognitionEvent>('activity-recognition', changeHandler);
 });
